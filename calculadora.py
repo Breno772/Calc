bc2345382aeed5b02e8234f7017482f4f44c76d8
〇 ᑭᖇ〇ᑭᖇƗ〇 ᙖ〇Ƭ〇 <breno017campos@gmail.com>
2025-04-03 00:10:43 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+x = float(input("Digite um número: "))
+y = float(input("Digite outro número: "))
+
+s = x+y
+
+print("Resultado da soma é:", s)
+
+m = x*y
+
+print("Resultado da multiplicação é:", m)
+
+d = x/y
+
+print("Resultado da divisão é:", d)
+
+sb = x-y
+
+print("Resultado da subtração é:", sb)
